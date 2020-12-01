467147c96f6c5d32f2f4ff37ca17a6bc49c586fb
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2020-12-01 13:54:42 -0300

    Agregada fuente Fira Code!

--- macOS/Scripts/InstalarFuentes.sh
+++ macOS/Scripts/InstalarFuentes.sh
@@ -11,3 +11,8 @@ mkdir $CARPETA/$NOMBREFUENTE
 unzip $CARPETA/$NOMBREFUENTE.zip -d $CARPETA/$NOMBREFUENTE/
 cp -R $CARPETA/$NOMBREFUENTE/TTF/*.ttf /Library/Fonts
 
+# Fira Code
+echo "Instalando Fira Code..."
+NOMBREFUENTE='FiraCode'
+git clone https://github.com/tonsky/FiraCode $CARPETA/$NOMBREFUENTE
+cp -R $CARPETA/$NOMBREFUENTE/distr/ttf/*.ttf /Library/Fonts
