2c816ee0e33ee8d399241ca4b75d6e294ed11ad9
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2020-12-02 00:39:42 -0300

    3 fuentes descargándose correctamente!

--- macOS/Scripts/InstalarFuentes.sh
+++ macOS/Scripts/InstalarFuentes.sh
@@ -3,6 +3,10 @@ echo 'Instalando fuentes...'
 CARPETA='FuentesDescargadas'
 echo Carpeta elegida $CARPETA
 
+# Limpiar carpeta...
+echo 'Limpiando Carpeta de descargas'
+rm -Rf $CARPETA
+
 # Victor Mono
 echo "Instalando Victor Mono..."
 NOMBREFUENTE='VictorMonoAll'
@@ -17,9 +21,10 @@ echo "Instalando $NOMBREFUENTE..."
 git clone https://github.com/tonsky/FiraCode $CARPETA/$NOMBREFUENTE
 cp -R $CARPETA/$NOMBREFUENTE/distr/ttf/*.ttf /Library/Fonts
 
+# Cascadia
 NOMBREFUENTE='Cascadia'
 echo "Instalando $NOMBREFUENTE..."
-NOMBREARCHIVO=CascadiaCode-2008.25.zip
-wget https://github.com/microsoft/cascadia-code/releases/download/v2008.25/$ARCHIVO -P $CARPETA
+NOMBREARCHIVO=CascadiaCode-2009.22.zip
+wget https://github.com/microsoft/cascadia-code/releases/download/v2009.22/$NOMBREARCHIVO -P $CARPETA
 unzip $CARPETA/$NOMBREARCHIVO -d $CARPETA/$NOMBREFUENTE/
 cp -R $CARPETA/$NOMBREFUENTE/ttf/*.ttf /Library/Fonts
